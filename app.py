59005c97cda422f4763e15058bc7981945a94af9
Manisha Patwal <manishapatwal34@gmail.com>
2025-06-11 02:42:02 +0530

    commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+EMBEDDINGS = download_hugging_face_embeddings()
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following context to answer the question. "
+    "If you don't know the answer, say that "
+    "you don't know. Use three sentences maximum and "
+    "Keep your answer concise."
+    "\n\n"
+    "\n{context}"
+)
